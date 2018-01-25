11f8418b97a5cfe9a2a0712cc05fe242821f49de
joliph <zhjf1996@163.com>
2018-01-25 18:20:18 +0800

    cnn复习

---
+++ cnn.py
@@ -0,0 +1,80 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+mnist=input_data.read_data_sets("MNIST_data/",one_hot=True)
+sess=tf.InteractiveSession()
+
+def weight_Variable(shape):
+    #tf.truncated_normal 截断的产生正太分布的函数
+    #tf.Variable(initializer， name)
+    return tf.Variable(tf.truncated_normal(shape,stddev=0.1))
+
+def bias_Variable(shape):
+    #tf.constant(value, dtype=None, shape=None, name='Const') 生成常数
+    return tf.Variable(tf.constant(0.1,shape=shape))
+
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+
+x=tf.placeholder(tf.float32,[None,784])
+y_=tf.placeholder(tf.float32,[None,10])
+x_image=tf.reshape(x,[-1,28,28,1])
+
+W_conv1=weight_Variable([5,5,1,32])
+b_conv1=bias_Variable([32])
+h_conv1=tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)
+h_pool1=max_pool_2x2(h_conv1)
+
+
+W_conv2=weight_Variable([5,5,32,64])
+b_conv2=bias_Variable([64])
+h_conv2=tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)
+h_pool2=max_pool_2x2(h_conv2)
+
+W_fc1=weight_Variable([7*7*64,1024])
+b_fc1=bias_Variable([1024])
+h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64])
+h_fc1=tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
+
+drop_rate=tf.placeholder(tf.float32)
+h_fc1_drop=tf.nn.dropout(h_fc1,keep_prob=drop_rate)
+
+w_fc2=weight_Variable([1024,10])
+b_fc2=bias_Variable([10])
+y_predict=tf.nn.softmax(tf.matmul(h_fc1_drop,w_fc2)+b_fc2)
+
+
+cross_entropy=tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y_predict),reduction_indices=[1]))
+train_step=tf.train.AdamOptimizer(1e-4).minimize(loss=cross_entropy)
+
+correct_prediction=tf.equal(tf.argmax(y_predict,1),tf.argmax(y_,1))
+accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+tf.global_variables_initializer().run()
+
+for i in range(20000):
+    batch=mnist.train.next_batch(50)
+    if i%100==0:
+        #eval()也是启动计算的一种方式和run差不多
+        train_accuracy=accuracy.eval(feed_dict={x:batch[0],y_batch[1],drop_rate=1.0)
+        print("step %d,training accuracy %g"%(i,train_accuracy))
+    train_step.run(feed_dict={x:batch[0],y_batch[1],drop_rate=0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels,drop_rate=1.0}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
